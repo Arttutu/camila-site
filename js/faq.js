6c46b1a17b468967effc5dbfbfdbd98995173a49
Arthur <arthurtt08@gmail.com>
2023-01-05 19:01:52 -0300

    faq

--- js/faq.js
+++ js/faq.js
@@ -6,21 +6,21 @@ $(document).ready(function () {
 
   $(".seta-baixo").click(function () {
     if (contador % 2 == 0) {
-      $(".resposta1").fadeIn(1000)
-      $(".seta-baixo").attr("class", "seta-baixo bi bi-arrow-up-circle")
+      $(".resposta1").slideDown(1000)
+      $(".seta-baixo").attr("class", "seta-baixo bi bi-x")
     } else {
-      $(".resposta1").fadeOut(1000)
-      $(".seta-baixo").attr("class", "seta-baixo bi bi-arrow-down-circle")
+      $(".resposta1").slideUp(1000)
+      $(".seta-baixo").attr("class", "seta-baixo bi bi-plus")
     }
     contador++
   })
   $(".seta-baixo2").click(function () {
     if (contador2 % 2 == 0) {
-      $(".resposta2").fadeIn(1000)
-      $(".seta-baixo2").attr("class", "seta-baixo2 bi bi-arrow-up-circle")
+      $(".resposta2").slideDown(1000)
+      $(".seta-baixo2").attr("class", "seta-baixo2 bi bi-x")
     } else {
-      $(".resposta2").fadeOut(1000)
-      $(".seta-baixo2").attr("class", "seta-baixo2 bi bi-arrow-down-circle")
+      $(".resposta2").slideUp(1000)
+      $(".seta-baixo2").attr("class", "seta-baixo2 bi bi-plus")
     }
     contador2++
   })
