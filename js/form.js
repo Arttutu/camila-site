20445eea4483116cd68c85ac206af7ed96cb6b4d
Arthur <arthurtt08@gmail.com>
2023-01-11 20:48:54 -0300

    teste

--- js/form.js
+++ js/form.js
@@ -1,26 +1,8 @@
-$("#form").validate({
-  rules: {
-    nome: {
-      required: true,
-      minlength: 3,
-    },
-    email: {
-      required: true,
-    },
-    mensagem: {
-      required: true,
-    },
-  },
-  messages: {
-    nome: {
-      required: "Por favor, informe seu nome",
-      minlength: "O nome deve ter pelo menos 3 caracteres",
-    },
-    email: {
-      required: "É necessário informar um email",
-    },
-    mensagem: {
-      required: "A mensagem não pode ficar em branco",
-    },
-  },
+$("#btn-enviar").click(function () {
+  var form = $("#form")
+  $.ajax({
+    url: "https://formsubmit.co/ajax/arthurtt08@gamil.com",
+    method: "POST",
+    data: form.serialize(),
+  })
 })
