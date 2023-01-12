a04f94d3e068dc3503a10d2cf8c4330b7b5f7b7f
Arthur <arthurtt08@gmail.com>
2023-01-12 20:54:02 -0300

    validate

--- js/form.js
+++ js/form.js
@@ -1,8 +1,25 @@
-$("#btn-enviar").click(function () {
-  var form = $("#form")
-  $.ajax({
-    url: "https://formsubmit.co/ajax/arthurtt08@gamil.com",
-    method: "POST",
-    data: form.serialize(),
+$(document).ready(function () {
+  $("#form").validate({
+    rules: {
+      nome: {
+        required: true,
+        maxlength: 40,
+        minlength: 3,
+        minWords: 2,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      numero: {
+        number: true,
+        range: [9, 11],
+      },
+      errorPlacement: function (label, element) {
+        label.addClass("arrow")
+        label.insertAfter(element)
+      },
+      wrapper: "span",
+    },
   })
 })
