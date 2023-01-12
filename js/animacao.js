409a1ac1597d56526c53ff4193cc7a6c391229b6
Arthur <arthurtt08@gmail.com>
2023-01-11 23:37:39 -0300

    animacao

--- js/animacao.js
+++ js/animacao.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
   $("#info").fadeOut(0)
   $("#info").fadeIn(2000)
+  $(".header_area").fadeOut(0)
+  $(".header_area").fadeIn(2000)
+  $(".cabecalho").fadeOut(0)
+  $(".cabecalho").fadeIn(2000)
 })
