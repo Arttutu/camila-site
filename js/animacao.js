4cc6af6da82637fc06cc3be349ddbb28a26568d3
Arthur <arthurtt08@gmail.com>
2023-01-18 17:28:43 -0300

    finalizando-principal

--- js/animacao.js
+++ js/animacao.js
@@ -6,3 +6,21 @@ $(document).ready(function () {
   $(".cabecalho").fadeOut(0)
   $(".cabecalho").fadeIn(1000)
 })
+var $target = $(".anime"),
+  animationClass = "anime-start",
+  offset = ($(window).height() * 3) / 4
+
+function animeScroll() {
+  var documentTop = $(document).scrollTop()
+
+  $target.each(function () {
+    var itemTop = $(this).offset().top
+    if (documentTop > itemTop - offset) {
+      $(this).addClass(animationClass)
+    } else {
+      $(this).removeClass(animationClass)
+    }
+  })
+}
+
+animeScroll()
